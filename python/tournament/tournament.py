557c286fd085000c331955deb4aa67f80ce3affa
Ådne <aadne.karstad@gmail.com>
2019-03-31 17:51:58 +0200

    Solved the exercise.

--- python/tournament/tournament.py
+++ python/tournament/tournament.py
@@ -1,18 +1,23 @@
 def tally(tournament_results):
     rv = 'Team                           | MP |  W |  D |  L |  P'
+
+    if tournament_results == '':
+        return rv
     
     POS = 0             # Position for the current team.
     tmt = []            # list of teams in the tournment.
     team_location = {}  # location of the team in the list above.
+
+    tournament_results = tournament_results.split('\n')
     for line in tournament_results:
         game = line.split(';')
         for tmt_team in range(2):
             if not any(_['team'] == game[tmt_team] for _ in tmt):
                 tmt.append({'team': game[tmt_team], 'MP': 0, 'W': 0, 'D': 0, 'L': 0, 'P': 0})
-                team_location[tmt_team] = POS
+                team_location[game[tmt_team]] = POS
                 POS += 1
-        if game[3] == 'draw':
-            pos_one, pos_two = team_location[game[1]], team_location[game[2]]
+        if game[2]== 'draw':
+            pos_one, pos_two = team_location[game[0]], team_location[game[1]]
             ''' Increment matches played, draw and points. '''
             tmt[pos_one]['MP'] += 1
             tmt[pos_one]['D'] += 1
@@ -22,10 +27,10 @@ def tally(tournament_results):
             tmt[pos_two]['D'] += 1
             tmt[pos_two]['P'] += 1
         else:
-            if game[3] == 'win':
-                pos_winner, pos_loser = team_location[game[1]], team_location[game[2]]
-            elif game[3] == 'loss':
-                pos_winner, pos_loser = team_location[game[2]], team_location[game[1]]
+            if game[2] == 'win':
+                pos_winner, pos_loser = team_location[game[0]], team_location[game[1]]
+            elif game[2] == 'loss':
+                pos_winner, pos_loser = team_location[game[1]], team_location[game[0]]
 
             ''' Winner increments Matches played and Wins: and gains three points. '''
             tmt[pos_winner]['MP'] += 1
@@ -34,10 +39,15 @@ def tally(tournament_results):
             ''' Losers increment Matches played and losses: gains zero points. '''
             tmt[pos_loser]['MP'] += 1
             tmt[pos_loser]['L'] += 1
-    print(sorted(tmt, key = lambda i: i['P'], reverse=True))
-        # print(team)
-        # rv += '{}{}{} | {} | {} | {} | {}'.format(tm['Team'], ' '*(30 - len(tm['team']), tm['MP'], tm['W'], tm['D'], tm['L'], tm['P']))
-    
+    for team in sorted(tmt, key = lambda i: (-i['P'], i['team']), reverse=False):
+        spaces = ' '*(31 - len(team['team']))
+        rv += '\n{}{}|  {} |  {} |  {} |  {} |  {}'.format(
+                                                team['team'],
+                                                spaces,
+                                                str(team['MP']),
+                                                str(team['W']),
+                                                str(team['D']),
+                                                team['L'],
+                                                str(team['P'])
+                                            )
     return rv
-
-print(tally('Allegoric Alaskans;Blithering Badgers;win'))
