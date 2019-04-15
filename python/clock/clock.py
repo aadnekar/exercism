e3ee3cae71ba72af9b432706f8adc4ed5870402d
Ådne <aadne.karstad@gmail.com>
2019-04-15 22:07:28 +0200

    I've completed a few exercises

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,15 +1,18 @@
 class Clock(object):
+    MINUTES_PER_HOUR = 60
+    HOURS_PER_DAY = 24
     def __init__(self, hour, minute):
-        pass
+        self.minutes = (minute + hour * 60) % self.MINUTES_PER_HOUR
+        self.hours = ((minute + hour * 60) // self.MINUTES_PER_HOUR) % self.HOURS_PER_DAY
 
     def __repr__(self):
-        pass
+        return '{:02d}:{:02d}'.format(self.hours, self.minutes)
 
     def __eq__(self, other):
-        pass
+        return self.minutes == other.minutes and self.hours == other.hours
 
-    def __add__(self, minutes):
-        pass
+    def __add__(self, minute):
+        return self.__class__(self.hours, self.minutes + minute)
 
-    def __sub__(self, minutes):
-        pass
+    def __sub__(self, minute):
+        return self.__class__(self.hours, self.minutes - minute)
--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,15 @@
 class Luhn(object):
     def __init__(self, card_num):
-        pass
+        self.card_num = card_num.replace(' ', '')
 
     def is_valid(self):
-        pass
+        if len(self.card_num) < 2 or not self.card_num.isdigit():
+            return False
+
+        digits = list(self.card_num)
+
+        for _ in range(len(digits)-2, -1, -2):
+            digits[_] = int(digits[_]) * 2
+            digits[_] = digits[_] if digits[_] < 10 else digits[_] - 9
+        
+        return sum(int(_) for _ in digits) % 10 == 0
